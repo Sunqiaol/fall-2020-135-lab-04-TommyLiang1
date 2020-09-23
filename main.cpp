f731c504bb148074bdffaac590aa903f9026449f
TommyLiang1 <tliang4680@bths.edu>
2020-09-23 16:48:51 -0400

    Updated Files

--- main.cpp
+++ main.cpp
@@ -4,26 +4,19 @@
 
 int main()
 {
-  /*
   std::string rect = box(3,7);
   std::cout << "7 by 3 rectangle\n";
   std::cout << rect;
   std::cout << "4 by 4 square\n" << box(4,4);
   std::cout << "3 by 7 rectangle\n" << box(7,3);
-  */
 
-  /*
   std::cout << "6 by 11 checkerboard\n" << checkerboard(11, 6);
   std::cout << "3 by 7 checkerboard\n" << checkerboard(7, 3);
   std::cout << "5 by 5 checkerboard\n" << checkerboard(5, 5);
-  */
 
-  /*
   std::cout << "8 cross\n" << cross(8);
   std::cout << "5 cross\n" << cross(5);
-  */
 
-  /*
   std::cout << "lower triangle 6\n" << lower(6);
   std::cout << "lower triangle 4\n" << lower(4);
   std::cout << "lower triangle 5\n" << lower(5);
@@ -31,13 +24,10 @@ int main()
   std::cout << "upper triangle 6\n" << upper(6);
   std::cout << "upper triangle 4\n" << upper(4);
   std::cout << "upper triangle 5\n" << upper(5);
-  */
 
-  /*
-  std::cout << "upside-down trapezoid 12, 5\n" << trapezoid(12a, 5);
+  std::cout << "upside-down trapezoid 12, 5\n" << trapezoid(12, 5);
   std::cout << "upside-down trapezoid 12, 7\n" << trapezoid(12, 7);
   std::cout << "upside-down trapezoid 15, 4\n" << trapezoid(15, 4);
-  */
 
   std::cout << "checkerboard3x3 16, 11\n" << checkerboard3x3(16, 11);
   std::cout << "checkerboard3x3 15, 15\n" << checkerboard3x3(15, 15);
